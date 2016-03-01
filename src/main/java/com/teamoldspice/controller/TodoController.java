d17f33e1a2100b66d3ecbdcd6ca1857328015c8d
Stephan February + Alan Yeo <pair+sf+ay@pivotal.io>
2016-03-01 17:35:41 +0800

    List all todos with templating engine

--- src/main/java/com/teamoldspice/controller/TodoController.java
+++ src/main/java/com/teamoldspice/controller/TodoController.java
@@ -4,27 +4,25 @@ import com.teamoldspice.model.Todo;
 import com.teamoldspice.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class TodoController {
+
+    public static final String INDEX_VIEW = "index";
 
     @Autowired
     TodoRepository repository;
 
     @RequestMapping("/")
-    public String hello(){
-
-        ArrayList<String> todoStrings = new ArrayList<String>();
-
-        for(Todo todo: repository.findAll()) {
-            todoStrings.add(todo.getName());
-        }
-
-        return todoStrings.toString();
+    public String index(Model model) {
+        model.addAttribute("todoList", repository.findAll());
+        return INDEX_VIEW;
     }
 }
