a4e1bf4943ea465734fa2b697f0d09690e297ae1
Stephan February + Yifeng Hou <pair+sf+yh@pivotal.io>
2016-03-15 14:16:07 +0800

    full resource server implementation is works

---
+++ src/main/java/com/teamoldspice/config/ResourceConfiguration.java
@@ -0,0 +1,51 @@
+package com.teamoldspice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import java.util.Arrays;
+
+/**
+ * Created by neo on 15/3/16.
+ */
+@Configuration
+@EnableOAuth2Client
+public class ResourceConfiguration {
+
+
+    @Bean
+    public OAuth2ProtectedResourceDetails todoAuthServerResourceDetails() {
+        AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+        details.setId("auth/server");
+        details.setClientId("todo");
+        details.setClientSecret("todopassword");
+        details.setAccessTokenUri("http://localhost:9999/uaa/oauth/token");
+        details.setUserAuthorizationUri("http://localhost:9999/uaa/oauth/authorize");
+        details.setScope(Arrays.asList("openid"));
+
+        return details;
+    }
+
+//    @Bean
+//    public OAuth2RestTemplate todoAuthRestTemplate(OAuth2ClientContext clientContext) {
+//        return new OAuth2RestTemplate(todoAuthServerResourceDetails(), clientContext);
+//    }
+
+    @Bean public OAuth2RestOperations restTemplate() {
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+
+        return new OAuth2RestTemplate(todoAuthServerResourceDetails(), new DefaultOAuth2ClientContext(atr));
+    }
+
+}
--- src/main/java/com/teamoldspice/config/ResourceServer.java
+++ src/main/java/com/teamoldspice/config/ResourceServer.java
@@ -1,12 +1,15 @@
 package com.teamoldspice.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 public class ResourceServer extends ResourceServerConfigurerAdapter{
@@ -24,5 +27,14 @@ public class ResourceServer extends ResourceServerConfigurerAdapter{
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception{
         resources.resourceId("todos");
+    }
+
+    @Bean
+    public ResourceServerTokenServices tokenService() {
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setClientId("todo");
+        tokenServices.setClientSecret("todopassword");
+        tokenServices.setCheckTokenEndpointUrl("http://localhost:9999/uaa/oauth/check_token");
+        return tokenServices;
     }
 }
